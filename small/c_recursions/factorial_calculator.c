13119a31e36eb58581a76500a5ee4f136fc6be40
enzeyi <emmanuelsiemaenzeyi@gmail.com>
2023-06-07 12:50:47 +0300

    factorial_calculator

---
+++ small/c_recursions/factorial_calculator.c
@@ -0,0 +1,27 @@
+//including the standard input/output library because I will use some of it's functions like scanf() and printf()
+#include <stdio.h>
+
+
+//declaring variables here
+
+//declaring functions here
+int factorial ();
+//initialising variables here
+int factorial () {
+
+}
+//defining functions here
+
+//main program here
+
+int main(){
+//asking the user to enter any number that they want to calculate it's factorial value
+//this input has to be a positive number, it should be 1 or more than 1
+//If the user enters, a non-integer or a non-positive number, the program should warn them
+printf("Please input the number that you would like to find it's factorial value then press enter:(The number has to be a positive one, IE 1 or more than 1) ");
+return 0;
+}
+
+
+
+
